3aebe2783ed4f384fed3c7f027ea917b1c9dcda6
Lucas Zischler <zischler1@gmail.com>
2020-12-30 14:14:25 -0300

    Update: Fixed some sizes for mobile display

--- php/getBody.php
+++ php/getBody.php
@@ -4,13 +4,34 @@ switch($lang):
 	case "en_us":
 		switch($position):
 			case "donate":
-				echo "Consider donating to these organizations:";
+				echo "Consider supporting these organizations:";
 				break;
 			case "posts":
 				echo "Posts";
 				break;
+			case "more":
+				echo "...More";
+				break;
 			case "sidebar":
 				echo "Info";
+				break;
+			case "ongoing_work":
+				echo "Works Ongoing";
+				break;
+			case "done_work":
+				echo "Works Concluded";
+				break;
+			case "availability":
+				echo "Availability";
+				break;
+			case "full":
+				echo "Full";
+				break;
+			case "free":
+				echo "Free";
+				break;
+			case "last_updates":
+				echo "Last Updates";
 				break;
 			case "port":
 				echo "Portifolio";
@@ -18,8 +39,23 @@ switch($lang):
 			case "about":
 				echo "About";
 				break;
+			case "biography":
+				echo "Biography";
+				break;
+			case "institutions":
+				echo "Institutions";
+				break;
 			case "cont":
 				echo "Contact";
+				break;
+			case "number":
+				echo "Phone";
+				break;
+			case "copyleft-name":
+				echo "Lucas Zischler, Personal Website";
+				break;
+			case "copyleft-notice":
+				echo "Copyleft: This is a free work, you can copy, distribute, and modify it under the terms of the <a href=\"http://artlibre.org/licence/lal/en/\">Free Art License</a>";
 				break;
 			default:
 				break;
@@ -28,13 +64,34 @@ switch($lang):
 	default:
 		switch($position):
 			case "donate":
-				echo "Considere fazer uma doação para estas organizações:";
+				echo "Considere apoiar estas organizações:";
 				break;
 			case "posts":
 				echo "Postagens";
 				break;
+			case "more":
+				echo "...Mais";
+				break;
 			case "sidebar":
 				echo "Informações";
+				break;	
+			case "ongoing_work":
+				echo "Trabalhos em Andamento";
+				break;
+			case "done_work":
+				echo "Projetos Concluídos";
+				break;
+			case "availability":
+				echo "Disponibilidade";
+				break;
+			case "full":
+				echo "Cheio";
+				break;
+			case "free":
+				echo "Livre";
+				break;
+			case "last_updates":
+				echo "Últimas Atualizações";
 				break;
 			case "port":
 				echo "Portifólio";
@@ -42,8 +99,23 @@ switch($lang):
 			case "about":
 				echo "Sobre";
 				break;
+			case "biography":
+				echo "Biografía";
+				break;
+			case "institutions":
+				echo "Instituições";
+				break;
 			case "cont":
 				echo "Contato";
+				break;
+			case "number":
+				echo "Telefone";
+				break;
+			case "copyleft-name":
+				echo "Lucas Zischler, Site Pessoal";
+				break;
+			case "copyleft-notice":
+				echo "Copyleft: Esta obra é livre, você pode copiar, compartilhar e modificar sob os termos da <a href=\"http://artlibre.org/licence/lal/en/\">Licença da Arte Livre</a>";
 				break;
 			default:
 				break;
